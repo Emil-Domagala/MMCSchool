d40d513f4a395fe54aa01ed13e0a0f3c7bbfa36d
Emil-Domagala <emil.domagalaa@gmail.com>
2022-10-03 22:59:25 +0200

    partly done accordeon

--- js/script.js
+++ js/script.js
@@ -1,51 +1,40 @@
 const navWrapper = document.querySelector(".nav__wrapper");
 const navBtn = document.querySelector(".burger-btn");
-//do w3School
-// var acc = document.querySelectorAll(".acordeon-section__btn");
-// var i;
-const acoSecText = document.querySelectorAll(".acordeon-section__hidden-text");
-//do moich opcji
-// const acoSecBtn = document.querySelectorAll(".acordeon-section__btn");
-
+const acc = document.querySelectorAll(".acordeon-section__btn");
+const plus = document.querySelectorAll(".fa-plus");
+const minus = document.querySelectorAll(".fa-minus");
+const faHide = document.querySelectorAll(".fa-hide");
 
 function showMenu() {
 	navWrapper.classList.toggle("nav__wrapper--active");
 }
 
-//opcja której nie do końca rozumiem, chyba z tablicą
-// const showHiddenText = () => {
-// 	acoSecText.classList.toggle("hidden-text-show");
-// 	acoSecBtn.forEach((item) => {
-// 		item.addEventListener("click", () => {
-// 			acoSecText.classList.remove("hidden-text-show");
-// 		});
-// 	});
-// };
+//Acordeon
 
-//łatwa opcja z funkcją liniową z dodaniem klasy max-height100%
-// acoSecBtn.forEach((item) =>
+let i;
+
+for (i = 0; i < acc.length; i++) {
+	acc[i].addEventListener("click", function () {
+		this.classList.toggle("button-active");
+		let panel = this.nextElementSibling;
+		if (panel.style.maxHeight) {
+			panel.style.maxHeight = null;
+		} else {
+			panel.style.maxHeight = panel.scrollHeight + "px";
+		}
+	});
+}
+
+
+
+
+//kewin
+// acoSecBtn.forEach((item) => {
 // 	item.addEventListener("click", () => {
-// 		acoSecText.classList.toggle("hidden-text-show");
-// 	})
-// );
-
-//najłatwiejsza opcja z dodaniem klasy z max-height100%
-// function showHiddenText() {
-// 	acoSecText.classList.toggle("hidden-text-show");
-// }
-
-//To znalazłem na w3schools i trochę przerobiłem
-// for (i = 0; i < acc.length; i++) {
-// 	acc[i].addEventListener("click", function () {
-// 		this.classList.toggle("hidden-text-show");
-// 		var panel = document.querySelectorAll(".acordeon-section__hidden-text");
-// 		if (panel.style.maxHeight) {
-// 			panel.style.maxHeight = null;
-// 		} else {
-// 			panel.style.maxHeight = panel.scrollHeight + "px";
-// 		}
+// 		let acordeon = item.querySelector(".acordeon-section__hidden-text");
+// 		console.log(acordeon);
+// 		acordeon.classList.toggle("hide-text-accordeon");
 // 	});
-// }
+// });
 
 navBtn.addEventListener("click", showMenu);
-// acoSecBtn.addEventListener("click", showHiddenText);
